dbb1ce36bdc051e897ffa5b6fc77436c3df1605d
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2023-09-19 23:44:34 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,18 @@
+// Node.js is an open-source, server-side JavaScript runtime environment that allows you to run JavaScript code on the server. 
+
+// Here are some key characteristics and features of Node.js:-
+
+// Non-blocking and Asynchronous
+// Server-Side Development
+// CommonJS Modules
+// NPM (Node Package Manager)
+// Cross-Platform
+// Community and Ecosystem
+// Real-Time and WebSocket Support
+// Microservices
+// Scalability
+// Serverless Computing
+
+// Node.js has become a popular choice for web developers, especially for building applications that require real-time capabilities, high concurrency, and efficient I/O operations.  
+
+// It's used by companies of all sizes to develop a wide range of applications, from web servers and APIs to microservices and IoT (Internet of Things) devices.
