35e68dafc52d4961902eb64e55521e83f022369e
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2023-10-01 12:45:17 +0530

    Add files via upload

---
+++ Hello World/helloWorld.js
@@ -0,0 +1,6 @@
+console.log("Hey There! i am Prince");
+
+// console.log(Window);
+// console.log(alert("Hey"));   These function of javaScript not included in node js.
+
+// Only the core part of javaScript is included in Node Js.
