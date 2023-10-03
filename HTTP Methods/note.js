1d8b701c32e6227aa607c73e3b733e6c211b3760
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2023-10-03 18:59:56 +0530

    Add files via upload

---
+++ HTTP Methods/note.js
@@ -0,0 +1,9 @@
+// HTTP methods are :
+// Get , Post , Put , Patch , Delete
+
+// 1. HTTP GET    : When you want some data from server.
+// 2. HTTP POST   : When you want to send and mutute some data in server.
+// 3. HTTP PUT    : When we want to upload picture or any file or data.
+// 4. HTTP PATCH  : when we want to change exesting entry.
+// 5. HTTP DELETE : when er want to delete data. Ex: you want to delete your account, etc  
+
